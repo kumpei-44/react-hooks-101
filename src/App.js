00c11636b8a7952bc091a83f7055a93674798309
kumpei hayashi <km.abs104@gmail.com>
2021-02-17 22:02:20 +0900

    my first React Hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const App = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount(count + 1);
+  const deincrement = () => setCount(count - 1);
+
+  const increment2 = () => setCount(previousCount => previousCount + 1);
+
+  const reset = () => setCount(0);
+
+  const double = () => setCount(count * 2);
+
+  const third = () => setCount(previousCount => {
+    if (previousCount % 3 === 0) {
+      return previousCount / 3;
+    } else {
+      return previousCount;
+    }
+  });
+
   return (
-    <div>
-      This is a template for React app.
-    </div>
+    <>
+      <div>count: {count}</div>
+      <div>
+        <button onClick={increment}>+1</button>
+        <button onClick={deincrement}>-1</button>
+      </div>
+      <div>
+        <button onClick={increment2}>+1</button>
+      </div>
+      <div>
+        <button onClick={reset}>Reset</button>
+        <button onClick={double}>×2</button>
+        <button onClick={third}>3の倍数のときだけ3で割る</button>
+      </div>
+    </>
   );
 }
 
