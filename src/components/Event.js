cedb6d7965fffe8f1141258cc1548234feaafc04
kumpei hayashi <km.abs104@gmail.com>
2021-02-20 14:36:53 +0900

    types-of-actions-like-constants

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import {DELETE_EVENT} from '../actions';
+
 const Event = ({event, dispatch}) => {
   const handleClickDelete = () => {
     const result = window.confirm(`id=${event.id}のイベントを本当に削除しても良いですか？`);
-    if (result) dispatch({type: 'DELETE_EVENT', id: event.id})
+    if (result) dispatch({type: DELETE_EVENT, id: event.id})
   }
   return (
     <tr>
